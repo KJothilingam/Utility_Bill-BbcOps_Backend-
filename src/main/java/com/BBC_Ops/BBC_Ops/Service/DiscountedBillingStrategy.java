f110662b50c882fa177a344d200650e78d3f5cb8
Lingam <kjothilingam06@gmail.com>
2025-03-30 16:57:40 +0530

    OverDue Filter

--- src/main/java/com/BBC_Ops/BBC_Ops/Service/DiscountedBillingStrategy.java
+++ src/main/java/com/BBC_Ops/BBC_Ops/Service/DiscountedBillingStrategy.java
@@ -8,6 +8,7 @@ public class DiscountedBillingStrategy implements BillingStrategy {
     private static final double STANDARD_RATE = 41.50;
     private static final double DISCOUNT = 0.05; // 5% discount
 
+
     @Override
     public double calculateBill(int unitConsumption) {
         double total = unitConsumption * STANDARD_RATE;
